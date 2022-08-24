5434d46573f253c39c246bc3526545163205848d
Milena Carolini Rodrigues <110571792+milenacr25@users.noreply.github.com>
2022-08-24 19:13:46 -0300

    Delete alfadivweb2 directory

--- alfadivweb2/2_tipos_de_dados/4_string/script.js
+++
@@ -1,9 +0,0 @@
-console.log("meunome é \n Milena"); // ASPAS DUPLAS 
-console.log('sou estudante'); // ASPAS SIMPLES (pode usar vareaveis dentro)
-console.log(`isso é um teste`);//CASE (pode fazer operações dentro)
-
-console.log(typeof("Meu nome é fulana"));
-console.log(typeof('sou estudante'));
-console.log(typeof(`isso é um teste`));
-
-
